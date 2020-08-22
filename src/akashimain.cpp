fbc9feed1374c0a2f41df3868807413c3c30784b
scatterflower <marisaposs@gameboyprinter.moe>
2020-08-22 13:33:51 -0500

    set up command line options

--- src/akashimain.cpp
+++ src/akashimain.cpp
@@ -1,11 +1,14 @@
-#include "akashimain.h"
+#include "include/akashimain.h"
 #include "ui_akashimain.h"
+
+#include <QDebug>
 
 AkashiMain::AkashiMain(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::AkashiMain)
 {
     ui->setupUi(this);
+    qDebug("Main application started");
 }
 
 AkashiMain::~AkashiMain()
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
-#include "akashimain.h"
+#include "include/akashimain.h"
 
 #include <QApplication>
+#include <QDebug>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+    QApplication::setApplicationName("akashi");
+    QApplication::setApplicationVersion("0.0.1");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("A server for Attorney Online 2");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    QCommandLineOption headlessOption(QStringList() << "l" << "headless", "Run the server without a GUI");
+    parser.addOption(headlessOption);
+
+    parser.process(app);
+    bool headless = parser.isSet(headlessOption);
+
     AkashiMain w;
-    w.show();
-    return a.exec();
+    if(!headless)
+        w.show();
+
+    return app.exec();
 }
