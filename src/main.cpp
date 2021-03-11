e60f7e6796c8f626efea59ca4179a86ab6792aab
in1tiate <radwoodward@vikings.grayson.edu>
2021-03-11 05:29:46 -0600

    update version string

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char* argv[])
 {
     QCoreApplication app(argc, argv);
     QCoreApplication::setApplicationName("akashi");
-    QCoreApplication::setApplicationVersion("0.0.1");
+    QCoreApplication::setApplicationVersion("1.0.0-alpha");
     std::atexit(cleanup);
 
     ConfigManager config_manager;
