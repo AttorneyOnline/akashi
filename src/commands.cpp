1636dd93ef60e5d4f782e1c23d03fdb171d3f22d
scatterflower <marisaposs@gameboyprinter.moe>
2021-03-17 05:52:04 -0500

    extremely important commit

--- src/commands.cpp
+++ src/commands.cpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////////////
 //    akashi - a server for Attorney Online 2                                       //
-//    Copyright (C) 2020  scatterflower                                           //
+//    Copyright (C) 2020  scatterflower                                             //
 //                                                                                  //
 //    This program is free software: you can redistribute it and/or modify          //
 //    it under the terms of the GNU Affero General Public License as                //
