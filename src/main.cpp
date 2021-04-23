5f07cbeff7904fdd082ab4eadf42c025bb99cc03
scatterflower <marisaposs@gameboyprinter.moe>
2021-04-23 00:32:44 -0500

    version bump

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char* argv[])
 {
     QCoreApplication app(argc, argv);
     QCoreApplication::setApplicationName("akashi");
-    QCoreApplication::setApplicationVersion("apricot r3 prerelease");
+    QCoreApplication::setApplicationVersion("banana");
     std::atexit(cleanup);
 
     ConfigManager config_manager;
