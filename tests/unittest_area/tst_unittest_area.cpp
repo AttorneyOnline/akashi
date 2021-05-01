025d8e0d1bf42f31c5dd5d70c154ea76dea18036
Cerapter <cerap@protonmail.com>
2021-05-01 23:12:40 +0200

    Add status tests

--- tests/unittest_area/tst_unittest_area.cpp
+++ tests/unittest_area/tst_unittest_area.cpp
@@ -20,6 +20,9 @@ private slots:
     void cleanup();
 
     void clientJoinLeave();
+
+    void areaStatuses_data();
+    void areaStatuses();
 };
 
 void Area::init()
@@ -44,6 +47,34 @@ void Area::clientJoinLeave()
 
     // No clients must be left in the area.
     QCOMPARE(m_area->charactersTaken().size(), 0);
+}
+
+void Area::areaStatuses_data()
+{
+    QTest::addColumn<QString>("statusCall");
+    QTest::addColumn<AreaData::Status>("expectedStatus");
+    QTest::addColumn<bool>("isSuccessful");
+
+    QTest::newRow("Idle") << "idle" << AreaData::Status::IDLE << true;
+    QTest::newRow("RP") << "rp" << AreaData::Status::RP << true;
+    QTest::newRow("Casing") << "casing" << AreaData::Status::CASING << true;
+    QTest::newRow("Looking for players (long)") << "looking-for-players" << AreaData::Status::LOOKING_FOR_PLAYERS << true;
+    QTest::newRow("Looking for players (short)") << "lfp" << AreaData::Status::LOOKING_FOR_PLAYERS << true;
+    QTest::newRow("Gaming") << "gaming" << AreaData::Status::GAMING << true;
+    QTest::newRow("Recess") << "recess" << AreaData::Status::RECESS << true;
+    QTest::newRow("Nonsense") << "blah" << AreaData::Status::IDLE << false;
+}
+
+void Area::areaStatuses()
+{
+    QFETCH(QString, statusCall);
+    QFETCH(AreaData::Status, expectedStatus);
+    QFETCH(bool, isSuccessful);
+
+    bool l_success = m_area->changeStatus(statusCall);
+
+    QCOMPARE(m_area->status(), expectedStatus);
+    QCOMPARE(l_success, isSuccessful);
 }
 
 }
