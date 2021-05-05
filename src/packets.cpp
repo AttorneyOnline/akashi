9b27774fd3cdd10e2ed5756083df3d589e67729e
Salanto <support@salanto.de>
2021-05-05 14:33:57 +0200

    Update packets.cpp
    Shouldn't send it if its garbage data

--- src/packets.cpp
+++ src/packets.cpp
@@ -64,8 +64,10 @@ void AOClient::pktSoftwareId(AreaData* area, int argc, QStringList argv, AOPacke
     sendPacket("PN", {QString::number(server->player_count), server->max_players});
     sendPacket("FL", feature_list);
 
+    if (server->asset_url.isValid()) {
     QByteArray asset_url = server->asset_url.toEncoded(QUrl::EncodeSpaces);
     sendPacket("ASS", {asset_url});
+    }
 }
 
 void AOClient::pktBeginLoad(AreaData* area, int argc, QStringList argv, AOPacket packet)
