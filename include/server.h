fb1caf6cee9e82e928be760c4affd05981514e17
Salanto <support@salanto.de>
2021-05-12 00:12:46 +0200

    Add optional content field for Webhook
    Allows the usage of pings with the webhook. Only added to the hook message if configured.

--- include/server.h
+++ include/server.h
@@ -317,10 +317,16 @@ class Server : public QObject {
      * @brief The minimum time between IC messages, in milliseconds.
      */
     int message_floodguard;
+
     /**
      * @brief URL send to the client during handshake to set the remote repository URL.
      */
     QUrl asset_url;
+
+    /**
+     * @brief Opional text to be send with the Discord embeed. Can be used to configure pings.
+     */
+    QString webhook_content;
 
   public slots:
     /**
