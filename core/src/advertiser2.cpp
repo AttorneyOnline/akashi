36273b76c94fff943a3b1b537d9cb53f513f416c
Salanto <support@salanto.de>
2021-05-20 23:54:21 +0200

    Implement basic advertiser
    Still missing the hook-up inside the server

--- core/src/advertiser2.cpp
+++ core/src/advertiser2.cpp
@@ -3,22 +3,49 @@
 Advertiser2::Advertiser2()
 {
     m_manager = new QNetworkAccessManager();
+    connect(m_manager, &QNetworkAccessManager::finished,
+            this, &Advertiser2::readReply);
 }
 
 void Advertiser2::advertiseServer()
 {
     if (m_masterserver.isValid()){
-        qDebug() << "Unable to advertise. Masterserver URL is not valid.";
+        qWarning() << "Unable to advertise. Masterserver URL '" + m_masterserver.toString() + "' is not valid.";
         return;
     }
+
+    QNetworkRequest request((QUrl (m_masterserver)));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    QJsonObject json;
+    json["port"] = m_port;
+    if (!(m_ws_port == -1)) {
+        json["ws_port"] = m_ws_port;
+    }
+
+    json["players"] = m_players;
+    json["name"] = m_name;
+
+    if (!(m_description.isEmpty())) {
+    json["description"] = m_description;
+    }
+
+    m_manager->post(request, QJsonDocument(json).toJson());
 }
 
 void Advertiser2::readReply(QNetworkReply *reply)
 {
-
+    QByteArray data = reply->readAll();
+    QString str_reply = data;
+    qDebug() << str_reply;
 }
 
 void Advertiser2::setAdvertiserSettings(QString f_name, QString f_description, int f_port, int f_ws_port, int f_players, QUrl f_master_url)
 {
-
+    m_name = f_name;
+    m_description = f_description;
+    m_port = f_port;
+    m_ws_port = f_ws_port;
+    m_players = f_players;
+    m_masterserver = f_master_url;
 }
