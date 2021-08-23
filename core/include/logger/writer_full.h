e5128976511ce00aa91de80b9afadff14c7a583d
Salanto <62221668+Salanto@users.noreply.github.com>
2021-08-23 17:02:48 +0200

    Add Full log writer

--- core/include/logger/writer_full.h
+++ core/include/logger/writer_full.h
@@ -18,14 +18,52 @@
 #ifndef WRITER_FULL_H
 #define WRITER_FULL_H
 #include <QObject>
+#include <QFile>
+#include <QDir>
+#include <QDateTime>
+#include <QTextStream>
 
+/**
+ * @brief A class for handling all log writing in the full logging mode.
+ */
 class WriterFull : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Constructor for full logwriter
+     *
+     * @details While this could've been a simple function, making it an object allows me to easier check runtime requirements
+     *          when reloading the logger. It also helps split the complex stucture of a logger into easier segments.
+     *
+     * @param QPObject pointer to the parent object.
+     */
     WriterFull(QObject* parent = nullptr);;
+
+    /**
+     * @brief Deconstructor for full logwriter.
+     *
+     * @details Doesn't really do anything, but its here for completeness sake.
+     */
     virtual ~WriterFull() {}
 
+public:
+    /**
+     * @brief Slot for u_logger to connect to when full logging is used.
+     * @param Preformatted QString which will be written into the logfile.
+     */
+    void flush(const QString f_entry);
+
+private:
+    /**
+     * @brief Filename of the logfile used. This will always be the time the server starts up.
+     */
+    QFile l_logfile;
+
+    /**
+     * @brief Directory where logfiles will be stored.
+     */
+    QDir l_dir;
 };
 
 #endif //WRITER_FULL_H
--- core/src/logger/writer_full.cpp
+++ core/src/logger/writer_full.cpp
@@ -20,5 +20,19 @@
 WriterFull::WriterFull(QObject* parent) :
     QObject(parent)
 {
+    l_dir.setPath("logs/");
+    if (!l_dir.exists()) {
+        l_dir.mkpath(".");
+    }
+}
 
+void WriterFull::flush(const QString f_entry)
+{
+    l_logfile.setFileName(QString("logs/%1.log").arg(QDate::currentDate().toString("yyyy-MM-dd")));
+    if (l_logfile.open(QIODevice::WriteOnly | QIODevice::Append)) {
+        QTextStream file_stream(&l_logfile);
+        file_stream << f_entry;
+    }
+    l_logfile.close();
 };
+
