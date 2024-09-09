3b395240c9fb9aec4c3330d76bcf8a3d35ebec46
stonedDiscord <Tukz@gmx.de>
2024-09-09 18:36:01 +0200

    only show chars who are swapping
    Co-Authored-By: Salanto <62221668+Salanto@users.noreply.github.com>

--- src/commands/area.cpp
+++ src/commands/area.cpp
@@ -476,9 +476,15 @@ void AOClient::cmdClearAreaMessage(int argc, QStringList argv)
 void AOClient::cmdWebfiles(int argc, QStringList argv)
 {
     const QVector<AOClient *> l_clients = server->getClients();
+    QStringList l_weblinks;
     for (AOClient *l_client : l_clients) {
-        if (l_client->areaId() == areaId()) {
-            sendServerMessage("Character files: https://attorneyonline.github.io/webDownloader/index.html?char=" + l_client->m_current_iniswap);
+        if (l_client->m_current_iniswap.isEmpty() || l_client->areaId() != areaId()) {
+            continue;
+        }
+        
+        if (l_client->character().toLower() != l_client->m_current_iniswap.toLower()) {
+            l_weblinks.append("https://attorneyonline.github.io/webDownloader/index.html?char=" + l_client->m_current_iniswap);
         }
     }
+    sendServerMessage("Character files:\n" + l_weblinks.join("\n"));
 }
