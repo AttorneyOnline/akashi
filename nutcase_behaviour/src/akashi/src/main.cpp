a8ebc5170f2ec5b16e76fa66c4e7baea6d6d27d3
Salanto <62221668+Salanto@users.noreply.github.com>
2025-12-10 02:29:33 +0100

    Autodeployment of external headers

--- nutcase_behaviour/src/akashi/src/main.cpp
+++ nutcase_behaviour/src/akashi/src/main.cpp
@@ -1,10 +1,14 @@
 #include <QCoreApplication>
 
+#include "serviceregistry.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
     QCoreApplication::setApplicationName("akashi");
     QCoreApplication::setApplicationVersion("jackfruit (1.9)");
+
+    ServiceRegistry *registry = new ServiceRegistry(&app);
 
     return app.exec();
 }
